16363d82991e8e5644ca4d26a68934ff79a688aa
Aditya-078 <200303105146@paruluniversity.ac.in>
2022-08-05 14:59:31 +0530

    Continue changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import Login from "./Login";
 import Header from './Header'
 import Home from "./Home";
 import Prime from "./Prime";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";import Checkout from "./Checkout";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";import Checkout from "./Checkout";
+import { auth } from "./firebase"
 
 function App() {
+
+  useEffect(() => {
+    //will only run when app component loads...
+
+  auth.onAuthStateChanged(authUser => {
+    console.log('THE USER IS >>>', authUser);
+
+    if(authUser) {
+    
+      //the user just logged in the user was logged in
+    
+    } 
+    else{
+      
+      //the user is logged out
+    }
+  })
+    
+  }, [])
+  
+
+
+
   return (<Router>
     <div className="app">
       
